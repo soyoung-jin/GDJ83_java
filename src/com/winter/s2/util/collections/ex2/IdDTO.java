96f793044c495c4053f6f96b429b3cdbf792254e
soyoung-jin <jthdud0501@gmail.com>
2024-06-07 17:47:53 +0900

    2024.06.07-2

--- src/com/winter/s2/util/collections/ex2/IdDTO.java
+++ src/com/winter/s2/util/collections/ex2/IdDTO.java
@@ -2,4 +2,41 @@ package com.winter.s2.util.collections.ex2;
 
 public class IdDTO {
 
+	private String name;
+	private String phonenum;
+	private String email;
+	private long addressnum;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhonenum() {
+		return phonenum;
+	}
+
+	public void setPhonenum(String phonenum) {
+		this.phonenum = phonenum;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getAddressnum() {
+		return addressnum;
+	}
+
+	public void setAddressnum(long addressnum) {
+		this.addressnum = addressnum;
+	}
+
 }
--- src/com/winter/s2/util/collections/ex2/IdService.java
+++ src/com/winter/s2/util/collections/ex2/IdService.java
@@ -1,5 +1,18 @@
 package com.winter.s2.util.collections.ex2;
 
+import java.util.Scanner;
+
 public class IdService {
+
+	private StringBuffer sb;
+	Scanner sc = new Scanner(System.in);
+
+	public IdService() {
+		this.sb = new StringBuffer();
+		this.sb.append("홍시영, 01011112222, hong@gamil.com, 11104,");
+		this.sb.append("변우석, 01011113333, woo@gamil.com, 10729,");
+		this.sb.append("권지용, 01011114444, kwon@gamil.com, 80818");
+
+	}
 
 }

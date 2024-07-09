0a08175477ff0d247662dddb59b3d72f4060d29b
soyoung-jin <jthdud0501@gmail.com>
2024-07-09 17:08:06 +0900

    2024.07.09

---
+++ src/com/jin/study/Ex6_19.java
@@ -0,0 +1,20 @@
+package com.jin.study;
+
+public class Ex6_19 {
+	// 클래스명이 Account라고 가정
+//	private int balance;
+//	public static final int MAX_BALANCE = 1000000;
+//	public static final int MIN_BALANXE= 0;
+//	
+//	
+//	public int getBalance() {
+//		return balance;
+//	}
+//	public void setBalance(int balance) {
+//		if(balance<Account.MIN_BALANXE || balance>Account.MAX_BALANCE) {
+//			return;
+//		}
+//		this.balance = balance;
+//	}
+
+}
